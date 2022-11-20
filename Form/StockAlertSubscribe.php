1f9a700c9e4597a60fb212c92e738fcbe8deef68
Franck Allimant <fallimant@openstudio.fr>
2022-11-20 23:00:58 +0100

    Fix "extra field error" message

--- Form/StockAlertSubscribe.php
+++ Form/StockAlertSubscribe.php
@@ -13,8 +13,14 @@
 namespace StockAlert\Form;
 
 use StockAlert\StockAlert;
+use Symfony\Component\Form\FormFactoryBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\ValidatorBuilder;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Thelia\Core\Translation\Translator;
 use Thelia\Form\BaseForm;
 
@@ -26,6 +32,14 @@ use Thelia\Form\BaseForm;
  */
 class StockAlertSubscribe extends BaseForm
 {
+    public function __construct(Request $request, EventDispatcherInterface $eventDispatcher, TranslatorInterface $translator, FormFactoryBuilderInterface $formFactoryBuilder, ValidatorBuilder $validationBuilder, TokenStorageInterface $tokenStorage, string $type = "Symfony\Component\Form\Extension\Core\Type\FormType", array $data = [], array $options = [])
+    {
+        // To prevent "extra_fields_message" in local/modules/StockAlert/Controller/StockAlertFrontOfficeController.php:35
+        $options['csrf_protection'] = false;
+
+        parent::__construct($request, $eventDispatcher, $translator, $formFactoryBuilder, $validationBuilder, $tokenStorage, $type, $data, $options);
+    }
+
     protected function buildForm()
     {
         $this->formBuilder
