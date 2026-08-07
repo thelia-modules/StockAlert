be634ac3d7453e166303333c616c9699b3ca653d
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2026-08-07 15:49:13 +0200

    Clean up copyright comments in StockAlertBackOfficeController
    Removed unnecessary copyright comments from the file.

--- Controller/StockAlertBackOfficeController.php
+++ Controller/StockAlertBackOfficeController.php
@@ -12,13 +12,6 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-/*      Copyright (c) OpenStudio */
-/*      email : dev@thelia.net */
-/*      web : http://www.thelia.net */
-
-/*      For the full copyright and license information, please view the LICENSE.txt */
-/*      file that was distributed with this source code. */
-
 namespace StockAlert\Controller;
 
 use StockAlert\Form\StockAlertConfig;
